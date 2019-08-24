c43a8a668be0e0c0e37df93f63cc8023fca92cbc
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2019-08-23 21:06:21 -0300

    Added all query controllers

---
+++ Server/Controllers/QueryByNameController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [Route("api/Pessoa/query/name")]
+    [ApiController]
+    public class QueryByNameController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public QueryByNameController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{search}")]
+        public async Task<ActionResult<IEnumerable<Pessoa>>> GetPessoasByName(string search)
+        {
+            if(search == null)
+            {
+                return BadRequest();
+            }
+            List<Pessoa> pessoas = await _context.Pessoas.ToListAsync();
+
+            List<Pessoa> filtered = new List<Pessoa>();
+
+            foreach(Pessoa p in pessoas)
+            {
+                if(p.Nome.Contains(search))
+                {
+                    filtered.Add(p);
+                }
+            }
+
+            return filtered;
+        }
+    }
+}
---
+++ Server/Controllers/QueryController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [Route("api/Pessoa/query")]
+    [ApiController]
+    public class QueryController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public QueryController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{search}")]
+        public async Task<ActionResult<IEnumerable<Pessoa>>> GetPessoasBySearch(string search)
+        {
+            if(search == null)
+            {
+                return BadRequest();
+            }
+            List<Pessoa> pessoas = await _context.Pessoas.ToListAsync();
+
+            List<Pessoa> filtered = new List<Pessoa>();
+
+            foreach(Pessoa p in pessoas)
+            {
+                if(p.Email.Contains(search) || p.Nome.Contains(search))
+                {
+                    filtered.Add(p);
+                }
+            }
+
+            return filtered;
+        }
+    }
+}
